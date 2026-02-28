f3a820b38e2aa80645494059c654ef26ace7803f
hoangbui <buithaihoang06@gmail.com>
2026-02-28 16:40:29 +0200

    final

--- frontend/src/components/ui/aspect-ratio.tsx
+++ frontend/src/components/ui/aspect-ratio.tsx
@@ -1,5 +1,17 @@
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+"use client";
 
-const AspectRatio = AspectRatioPrimitive.Root;
+import * as React from "react";
+
+const AspectRatio = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<"div"> & { ratio?: number }
+>(({ ratio = 1, style, ...props }, ref) => (
+  <div
+    ref={ref}
+    style={{ position: "relative", width: "100%", aspectRatio: String(ratio), ...style }}
+    {...props}
+  />
+));
+AspectRatio.displayName = "AspectRatio";
 
 export { AspectRatio };
